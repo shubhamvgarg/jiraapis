34559393406191b920d5958eed19404152b6506c
Shubham Garg <shubham07119garg@gmail.com>
2024-08-26 11:58:31 +0530

    adding cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import taskRouter from "./routes/task.route.js";
+import cors from 'cors';
 
 dotenv.config();
 let PORT = process.env.PORT || 5000;
@@ -16,6 +17,10 @@ mongoose
   });
 
 const app = express();
+app.use(cors())
+app.use(cors({
+  origin: 'https://jiraapis.onrender.com'
+}));
 app.use(express.json());
 app.listen(PORT, () => {
   console.log(`server listening on port 5000, http://localhost:${PORT}`);
